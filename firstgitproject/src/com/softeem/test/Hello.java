9b0b3091d004022da2bfc5bdeb5dd187e6b347ae
gzc <3270694178@qq.com>
2023-02-27 16:23:06 +0800

    第一次修改测试

--- firstgitproject/src/com/softeem/test/Hello.java
+++ firstgitproject/src/com/softeem/test/Hello.java
@@ -3,5 +3,6 @@ package com.softeem.test;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("git测试");
+        System.out.println("hello........");
     }
 }
